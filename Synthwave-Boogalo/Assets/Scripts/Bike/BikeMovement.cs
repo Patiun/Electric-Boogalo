4876a27fdaf99cea06f3d4f0c7c7cacaa64179e4
Remlik <Remlikmg@comcast.net>
2018-05-17 15:41:27 -0400

    Bike lean movement uses force and steering no longer perpetually drifts
    leaning still needs forward velocity scaling

---
+++ Synthwave-Boogalo/Assets/Scripts/Bike/BikeMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: Greg Kilmer
+ * Function: Contols the movement of a bike object. Intended for use with both player and enemy bikes.
+ * Last Updated: 5/17/2018
+ */
+
+public class BikeMovement : MonoBehaviour {
+	public GameObject bike;
+	public float accelerationRate;
+	public float decelerationRate;
+	public float topSpeed;
+
+	public float leanRate;
+
+	public float turnRate;
+	//public float maxTurn;
+
+	public Vector3 curVelocity;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		curVelocity = rb.velocity;
+		//Curb Velocity to topSpeed
+		if (rb.velocity.magnitude >= topSpeed) {
+			rb.velocity = rb.velocity.normalized* topSpeed;
+		}
+
+		//DEBUG
+		Debug.DrawRay(transform.position,transform.right*10,Color.red);
+	}
+
+	//Increase speed in forward direction
+	public void Accelerate() {
+		rb.AddForce (transform.forward * accelerationRate);
+	}
+
+	//Decrease speed in the forward direction
+	public void Decelerate() {
+		rb.AddForce (transform.forward * -decelerationRate);
+	}
+
+	//Turn left using handle bars
+	public void TurnLeft() {
+		float turnAmt = -turnRate * rb.velocity.magnitude;
+		transform.RotateAround (transform.position, transform.up, turnAmt);
+		rb.velocity = Quaternion.AngleAxis (turnAmt, transform.up) * rb.velocity;
+	}
+
+	//Turn right using handle bars
+	public void TurnRight() {
+		float turnAmt = turnRate * rb.velocity.magnitude;
+		transform.RotateAround (transform.position, transform.up, turnAmt);
+		rb.velocity = Quaternion.AngleAxis (turnAmt, transform.up) * rb.velocity;
+	}
+
+	//Slides bike left by leaning left
+	public void LeanLeft(float percentageOfLeanRate) {
+		//TODO revamp to use velocity scaling only in the forward direction
+		rb.AddForce ((transform.right + transform.up * Mathf.Tan (transform.rotation.eulerAngles.z)) * -leanRate * percentageOfLeanRate);
+	}
+
+	//Slides bike right by learning right
+	public void LeanRight(float percentageOfLeanRate) {
+		//TODO revamp to use velocity scaling only in the forward direction
+		rb.AddForce ((transform.right + transform.up * Mathf.Tan (transform.rotation.eulerAngles.z)) * leanRate * percentageOfLeanRate);
+	}
+}
---
+++ Synthwave-Boogalo/Assets/Scripts/WorldGeneration/Hex_Road.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: Greg Kilmer
+ * Function: Controls logic for generating hex based road units
+ * Last Updated: 5/17/2018
+ */
+
+public class Hex_Road : MonoBehaviour {
+
+	public GameObject roadPrefab;
+	public GameObject buildingPrefab;
+	public float buildingChance = 80.00f;
+	public float size = 1.0f;
+	public GameObject[] neighbors;
+	public LayerMask layerMask;
+
+	// Use this for initialization
+	void Start () {
+		neighbors = new GameObject[6];
+		for (int n = 0; n < 6; n++) {
+			neighbors [n] = null;
+		}
+		GatherNeighbors ();
+		GenerateMissingNeighbors ();
+	}
+
+	private void GatherNeighbors() {
+		for (int i = 0; i < 6; i++) {
+			RaycastHit hit;
+			Vector3 dir = Quaternion.AngleAxis (60 * i, transform.up) * transform.forward;
+			Debug.DrawRay (transform.position, dir * size, Color.red);
+			Physics.Raycast (transform.position, dir, out hit, size, layerMask.value);
+			if (hit.collider != null) {
+				Debug.Log (hit.collider.tag);
+				neighbors [i] = hit.collider.gameObject;
+			}
+		}
+	}
+
+
+	private void GenerateMissingNeighbors () {
+		for (int i = 0; i < 6; i++) {
+			if (neighbors [i] == null) {
+				Vector3 dir = Quaternion.AngleAxis (60 * i, transform.up) * transform.forward;
+				if (Random.Range (0, 100) <= buildingChance) {
+					GameObject newBuilding = Instantiate (buildingPrefab);
+					newBuilding.transform.position = transform.position + dir.normalized*size;
+					neighbors [i] = newBuilding;
+				} else {
+					GameObject newRoad = Instantiate (roadPrefab);
+					newRoad.transform.position = transform.position + dir.normalized*size;
+					neighbors [i] = newRoad;
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
