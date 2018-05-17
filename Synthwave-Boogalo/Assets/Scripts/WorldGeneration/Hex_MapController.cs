40f30fee32f20896264a62273d0f01619d374b3b
Remlik <Remlikmg@comcast.net>
2018-05-17 17:58:59 -0400

    Scene with Hex Gen and Bike made : PlayScene :bike:

---
+++ Synthwave-Boogalo/Assets/Scripts/WorldGeneration/Hex_MapController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex_MapController : MonoBehaviour {
+
+	public int numHexes = 0;
+	public int maxHexes = 100;
+	public bool canAddHexes = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canAddHexes && numHexes >= maxHexes) {
+			canAddHexes = false;
+		}
+	}
+}
--- Synthwave-Boogalo/Assets/Scripts/WorldGeneration/Hex_Road.cs
+++ Synthwave-Boogalo/Assets/Scripts/WorldGeneration/Hex_Road.cs
@@ -10,15 +10,18 @@ using UnityEngine;
 
 public class Hex_Road : MonoBehaviour {
 
+	public Hex_MapController mapController;
 	public GameObject roadPrefab;
 	public GameObject buildingPrefab;
+	public float buildingOffset = 0.5f;
 	public float buildingChance = 80.00f;
-	public float size = 1.0f;
+	public float hexSize = 1.0f;
 	public GameObject[] neighbors;
 	public LayerMask layerMask;
 
 	// Use this for initialization
 	void Start () {
+		mapController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<Hex_MapController> ();
 		neighbors = new GameObject[6];
 		for (int n = 0; n < 6; n++) {
 			neighbors [n] = null;
@@ -31,8 +34,8 @@ public class Hex_Road : MonoBehaviour {
 		for (int i = 0; i < 6; i++) {
 			RaycastHit hit;
 			Vector3 dir = Quaternion.AngleAxis (60 * i, transform.up) * transform.forward;
-			Debug.DrawRay (transform.position, dir * size, Color.red);
-			Physics.Raycast (transform.position, dir, out hit, size, layerMask.value);
+			//Debug.DrawRay (transform.position, dir * hexSize, Color.red);
+			Physics.Raycast (transform.position, dir, out hit, hexSize, layerMask.value);
 			if (hit.collider != null) {
 				Debug.Log (hit.collider.tag);
 				neighbors [i] = hit.collider.gameObject;
@@ -43,17 +46,31 @@ public class Hex_Road : MonoBehaviour {
 
 	private void GenerateMissingNeighbors () {
 		for (int i = 0; i < 6; i++) {
-			if (neighbors [i] == null) {
-				Vector3 dir = Quaternion.AngleAxis (60 * i, transform.up) * transform.forward;
-				if (Random.Range (0, 100) <= buildingChance) {
-					GameObject newBuilding = Instantiate (buildingPrefab);
-					newBuilding.transform.position = transform.position + dir.normalized*size;
-					neighbors [i] = newBuilding;
-				} else {
-					GameObject newRoad = Instantiate (roadPrefab);
-					newRoad.transform.position = transform.position + dir.normalized*size;
-					neighbors [i] = newRoad;
+			if (mapController.canAddHexes) {
+				if (neighbors [i] == null) {
+					Vector3 dir = Quaternion.AngleAxis (60 * i, transform.up) * transform.forward;
+					if (Random.Range (0, 100) <= buildingChance) {
+						GameObject newBuilding = Instantiate (buildingPrefab);
+						Vector3 newPos = transform.position + dir.normalized * hexSize;
+						newPos.y += buildingOffset;
+						newBuilding.transform.position = newPos;
+						neighbors [i] = newBuilding;
+						mapController.numHexes += 1;
+					} else {
+						GameObject newRoad = Instantiate (roadPrefab);
+						newRoad.transform.position = transform.position + dir.normalized * hexSize;
+						neighbors [i] = newRoad;
+						mapController.numHexes += 1;
+					}
 				}
+			} else if (neighbors [i] == null) {
+				Vector3 dir = Quaternion.AngleAxis (60 * i, transform.up) * transform.forward;
+				GameObject newBuilding = Instantiate (buildingPrefab);
+				Vector3 newPos = transform.position + dir.normalized * hexSize;
+				newPos.y += buildingOffset;
+				newBuilding.transform.position = newPos;
+				neighbors [i] = newBuilding;
+				mapController.numHexes += 1;
 			}
 		}
 	}
